fb33513fe6be835b32002a532369604fea020cb8
gerra <gerralizza@gmail.com>
2015-03-09 22:36:04 +0300

    little fixes (TODO: .so)

--- 1/lib/helpers.c
+++ 1/lib/helpers.c
@@ -2,17 +2,25 @@
 #include <errno.h>
 
 ssize_t read_(int fd, void *buf, size_t count) {
-    int total_read = read(fd, buf, count);
-    if (total_read < 0) {
+    int total_read = 0;
+    int now_read;
+    while ((now_read = read(fd, buf, count)) > 0 && total_read < count) {
+        total_read += now_read;
+    }
+    if (now_read < 0) {
         perror(errno);
     }
     return total_read;
 }
 
 ssize_t write_(int fd, const void *buf, size_t count) {
-    int total_writed = write(fd, buf, count);
-    if (total_writed < 0) {
+    int total_written = 0;
+    int now_written;
+    while ((now_written = write(fd, buf, count)) > 0 && total_written < count) {
+        total_written += now_written;
+    }
+    if (now_written < 0) {
         perror(errno);
     }
-    return total_writed;
+    return total_written;
 }
