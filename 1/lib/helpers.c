66f9b3955a91807c7edd889303e2f7301e993ac9
gerra <gerralizza@gmail.com>
2015-03-11 01:11:15 +0300

    fix with size

--- 1/lib/helpers.c
+++ 1/lib/helpers.c
@@ -4,7 +4,7 @@
 ssize_t read_(int fd, void *buf, size_t count) {
     int total_read = 0;
     int now_read;
-    while (total_read < count && (now_read = read(fd, buf, count)) > 0) {
+    while (total_read < count && (now_read = read(fd, buf, count - total_read)) > 0) {
         total_read += now_read;
     }
     if (now_read < 0) {
@@ -16,7 +16,7 @@ ssize_t read_(int fd, void *buf, size_t count) {
 ssize_t write_(int fd, const void *buf, size_t count) {
     int total_written = 0;
     int now_written;
-    while (total_written < count && (now_written = write(fd, buf, count)) > 0) {
+    while (total_written < count && (now_written = write(fd, buf, count - total_written)) > 0) {
         total_written += now_written;
     }
     if (now_written < 0) {
